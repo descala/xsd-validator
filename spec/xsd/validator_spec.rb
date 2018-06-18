cfa3656db39564361f18e898e03fffe6a0dc50d5
Jordi Torrents <jtorrents@ingent.net>
2018-06-18 12:17:09 +0200

    Update xsd-validator Gem.
    Added tests, adapted for usage in b2bws and b2bnet, and corrected a few
    errors.

--- spec/xsd/validator_spec.rb
+++ spec/xsd/validator_spec.rb
@@ -1,9 +1,70 @@
 RSpec.describe Xsd::Validator do
+
+  include Xsd::Validator
+
   it "has a version number" do
     expect(Xsd::Validator::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'knows the root namespace' do
+    doc = "<thing xmlns='asdf'>asdf</thing>"
+    expect(root_namespace(doc)).to eq('asdf')
+    doc = "<ns:thing xmlns:ns='asdf'>asdf</thing>"
+    expect(root_namespace(doc)).to eq('asdf')
+    doc = "junk"
+    expect { root_namespace(doc) }.to raise_error "Is not a valid XML"
+    doc = "<thing>asdf</thing>"
+    expect { root_namespace(doc) }.to raise_error "XML does not have a root namespace"
   end
+
+  it 'validates facture32' do
+    doc=File.read('spec/files/13506.facturae32.xml')
+    expect(root_namespace_xsd(doc)).to match(/xsd-validator\/lib\/xsd\/schemas\/facturae32.xsd/)
+    expect(xsd_validate!(doc)).to eq(true)
+    doc="<facturae:Facturae xmlns:facturae=\"http://www.facturae.es/Facturae/2009/v3.2/Facturae\"></facurae>"
+    expect { xsd_validate!(doc) }.to raise_error(/Missing child element/)
+  end
+
+  it 'validates facture321' do
+    doc=File.read('spec/files/facturae321.xml')
+    expect(xsd_validate!(doc)).to eq(true)
+  end
+
+  it 'validates signed facture32' do
+    doc=File.read('spec/files/47610.facturae32.jccm.xsig')
+    expect(xsd_validate!(doc)).to eq(true)
+  end
+
+  it 'validates facturae32 with Extensions' do
+    doc=File.read('spec/files/facturae32_with_extension.xml')
+    expect(xsd_validate!(doc)).to eq(true)
+  end
+
+  it 'validates signed facture31' do
+    doc=File.read('spec/files/facturae31.xml')
+    expect(xsd_validate!(doc)).to eq(true)
+  end
+
+  it 'validates signed facture30' do
+    doc=File.read('spec/files/facturae30.xml')
+    expect(xsd_validate!(doc)).to eq(true)
+  end
+
+  it 'validates SII documents' do
+    doc=File.read('spec/files/sii/sii-factura-emitida-v06.xml')
+    expect(xsd_validate!(doc)).to eq(true)
+    doc=File.read('spec/files/sii/sii-factura-emitida1.xml')
+    expect(xsd_validate!(doc)).to eq(true)
+  end
+
+  it 'raises error if namespace is unknown' do
+    doc = "<ns:thing xmlns:ns='asdf'>asdf</thing>"
+    expect { xsd_validate!(doc) }.to raise_error "Unknown namespace asdf"
+  end
+
+  it 'raises ValidationError for an invalid XML' do
+    doc=File.read('spec/files/facturae321_wrong.xml')
+    expect { xsd_validate!(doc) }.to raise_error(Xsd::Validator::ValidationError)
+  end
+
 end
--- xsd-validator.gemspec
+++ xsd-validator.gemspec
@@ -9,8 +9,8 @@ Gem::Specification.new do |spec|
   spec.authors       = ["David Escala"]
   spec.email         = ["descala@ingent.net"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
+  spec.summary       = %q{Validates XML files with XSD..}
+  spec.description   = %q{Validates XML files with XSD.}
   spec.homepage      = "TODO: Put your gem's website or public repo URL here."
   spec.license       = "MIT"
 
@@ -30,7 +30,11 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.16"
+  spec.add_development_dependency "bundler", "~> 1.15"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "byebug"
+  spec.add_development_dependency "guard-rspec"
+
+  spec.add_dependency "nokogiri"
 end
