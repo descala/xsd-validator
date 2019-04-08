2644a55df5bcb152c5b1b73b744b646f256898ae
David Escala <descala@ingent.net>
2019-04-08 20:21:42 +0200

    Automaticaly validate all available spec files
    Adds UBL OrderResponse instance

--- spec/xsd/validator_spec.rb
+++ spec/xsd/validator_spec.rb
@@ -17,60 +17,9 @@ RSpec.describe Xsd::Validator do
     expect { root_namespace(doc) }.to raise_error "XML does not have a root namespace"
   end
 
-  it 'validates facture32' do
-    doc=File.read('spec/files/13506.facturae32.xml')
-    expect(root_namespace_xsd(doc)).to match(/xsd-validator\/lib\/xsd\/schemas\/facturae32.xsd/)
-    expect(xsd_validate!(doc)).to eq(true)
+  it 'raises error' do
     doc="<facturae:Facturae xmlns:facturae=\"http://www.facturae.es/Facturae/2009/v3.2/Facturae\"></facurae>"
     expect { xsd_validate!(doc) }.to raise_error(/Missing child element/)
-  end
-
-  it 'validates facture321' do
-    doc=File.read('spec/files/facturae321.xml')
-    expect(xsd_validate!(doc)).to eq(true)
-  end
-
-  it 'validates signed facture32' do
-    doc=File.read('spec/files/47610.facturae32.jccm.xsig')
-    expect(xsd_validate!(doc)).to eq(true)
-  end
-
-  it 'validates facturae32 with Extensions' do
-    doc=File.read('spec/files/facturae32_with_extension.xml')
-    expect(xsd_validate!(doc)).to eq(true)
-  end
-
-  it 'validates signed facture31' do
-    doc=File.read('spec/files/facturae31.xml')
-    expect(xsd_validate!(doc)).to eq(true)
-  end
-
-  it 'validates signed facture30' do
-    doc=File.read('spec/files/facturae30.xml')
-    expect(xsd_validate!(doc)).to eq(true)
-  end
-
-  describe 'validates facturae322 with face b2b extension' do
-    it 'version 1.0' do
-      doc=File.read('spec/files/facturae322.xml')
-      expect(xsd_validate!(doc)).to eq(true)
-    end
-
-    it 'version 1.1' do
-      doc=File.read('spec/files/facturae322_fb2b11.xml')
-      expect(xsd_validate!(doc)).to eq(true)
-    end
-  end
-
-  it 'validates SII documents' do
-    doc=File.read('spec/files/sii/sii-factura-emitida-v06.xml')
-    expect(xsd_validate!(doc)).to eq(true)
-    doc=File.read('spec/files/sii/sii-factura-emitida1.xml')
-    expect(xsd_validate!(doc)).to eq(true)
-    doc=File.read('spec/files/sii/sii-factura-bizkaia.xml')
-    expect(xsd_validate!(doc)).to eq(true)
-    doc=File.read('spec/files/sii/sii-factura-gipuzkoa.xml')
-    expect(xsd_validate!(doc)).to eq(true)
   end
 
   it 'raises error if namespace is unknown' do
@@ -83,9 +32,11 @@ RSpec.describe Xsd::Validator do
     expect { xsd_validate!(doc) }.to raise_error(Xsd::Validator::ValidationError)
   end
 
-  it 'validates facture32 with signature and xades extensions' do
-    doc=File.read('spec/files/xades.xml')
-    expect(xsd_validate!(doc)).to eq(true)
+  it 'validates spec files' do
+    Dir["spec/files/**/*"].each do |filename|
+      next if filename =~ /wrong/
+      doc=File.read(filename) rescue next
+      expect(xsd_validate(doc)).to eq([]), "Error validating fixture #{filename}"
+    end
   end
-
 end
