c0bc1ed3df29a63734dca3397a3be3f01797cf44
David Escala <descala@ingent.net>
2019-11-06 18:56:39 +0100

    Use absolute paths

--- lib/sch/validator.rb
+++ lib/sch/validator.rb
@@ -12,13 +12,7 @@ module Sch
     def sch_validate(doc)
       errors = []
       schematrons(doc).each do |schematron_file|
-        cache_xslt = "lib/sch/compiled/#{File.basename(schematron_file)}.xslt"
-        if File.exists?(cache_xslt)
-          compiled_schematron = File.read(cache_xslt)
-        else
-          compiled_schematron = Schematron::XSLT2.compile(File.read(schematron_file))
-          File.write(cache_xslt, compiled_schematron)
-        end
+        compiled_schematron = File.read(xslt_path(schematron_file))
         validation_result = Schematron::XSLT2.validate(compiled_schematron, doc)
         errors = errors + Schematron::XSLT2.get_errors(validation_result)
       end
@@ -37,9 +31,9 @@ module Sch
       customization_id = doc_nokogiri.xpath('//cbc:CustomizationID', cbc: CBC).text
       case customization_id
       when 'urn:cen.eu:en16931:2017#compliant#urn:fdc:peppol.eu:2017:poacc:billing:3.0'
-        %w(lib/sch/schemas/CEN-EN16931-UBL.sch lib/sch/schemas/PEPPOL-EN16931-UBL.sch)
+        %w(CEN-EN16931-UBL.sch PEPPOL-EN16931-UBL.sch)
       when 'urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:aunz:3.0'
-        %w(lib/sch/schemas/AUNZ-UBL-validation.sch lib/sch/schemas/AUNZ-PEPPOL-validation.sch)
+        %w(AUNZ-UBL-validation.sch AUNZ-PEPPOL-validation.sch)
       else
         raise StandardError.new("Unkown CustomizationID '#{customization_id}'")
       end
@@ -54,6 +48,12 @@ module Sch
           File.write(cache_xslt, compiled_schematron)
         end
       end
+    end
+
+    private
+
+    def xslt_path(name)
+      File.expand_path("../compiled/#{name}.xslt", __FILE__)
     end
   end
 end
