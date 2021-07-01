9a9494eb17e624206efeb1ad579dfc9c05249862
Lluís Gili <lluis@ingent.net>
2021-07-01 10:24:22 +0200

    failing test for PT rule BR-CIUS-PT-18

--- spec/sch/validator_spec.rb
+++ spec/sch/validator_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe Sch::Validator do
     end
   end
 
+  it 'raises ValidationError for an invalid CIUS pt' do
+    doc=File.read('spec/files/sch/invoice-cius-pt-wrong.xml')
+    expect { sch_validate!(doc) }.to raise_error(Sch::Validator::ValidationError, /FATAL: .*BR-CIUS-PT-18/)
+  end
+
   it 'raises ValidationError for an invalid XML' do
     doc=File.read('spec/files/sch/invoice-se-wrong.xml')
     expect { sch_validate!(doc) }.to raise_error(Sch::Validator::ValidationError, /FATAL: .*BR-S-08/)
